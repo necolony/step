81f3cb00be11b1b23638cfdac3d288d63cf85e0c
necolony <mintybean7@gmail.com>
2020-03-25 10:58:45 +0900

    パスワードリセット機能追加

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
